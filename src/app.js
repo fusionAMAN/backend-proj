c0d087f482e18d0686de8875c3cf83ced88b1c24
fusionAMAN <786rockaman@gmail.com>
2025-02-16 12:18:45 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 
+
 const app=express()
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -11,4 +12,16 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended: true, limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+// routes import
+import userRouter from './routes/user.routes.js'
+
+
+//routes declaration
+
+// app.use("/users",userRouter)  //--> httpp://localhost:8000/users/register
+/*jab /users search hoga toh userRouter call hoga udhar /register hai fir vo active hoga */
+
+app.use("/api/v1/users",userRouter) //httpp://localhost:8000/api/v1/users/register
+
 export {app}
